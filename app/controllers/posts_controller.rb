6224b926b79747af3bd79bde242666d6d2dc412e
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-06 16:43:26 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,8 +24,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     binding.pry
-    if @post.valid?
-      @post.update(post_params)
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
