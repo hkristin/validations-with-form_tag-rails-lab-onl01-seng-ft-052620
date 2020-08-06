a2a84c3b126d978214d1c8667529e4d62fc32483
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-06 16:25:38 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  validates :name, length: { minimum: 2 }
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :phone_number, length: { is: 10 }
 end
